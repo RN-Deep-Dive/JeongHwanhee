7551dcd0519e7bfa01ef64c652981e8402eb69f4
hwanheejung <hwanheejung117@gmail.com>
2023-09-16 00:28:14 +0900

    kakao: ~11.정리

---
+++ kakaotalk-clone/src/FriendList.js
@@ -0,0 +1,25 @@
+import { ScrollView, View } from "react-native";
+import Profile from "./Profile";
+import { getBottomSpace } from "react-native-iphone-x-helper";
+import Margin from "./Margin";
+
+const bottomSpace = getBottomSpace();
+
+export default (props) => {
+  return (
+    props.isOpened && (
+      <ScrollView showsVerticalScrollIndicator={false}>
+        {props.data.map((item, index) => (
+          <View key={index}>
+            <Profile
+              uri={item.uri}
+              name={item.name}
+              introduction={item.introduction}
+            />
+            <Margin height={13} />
+          </View>
+        ))}
+      </ScrollView>
+    )
+  );
+};
